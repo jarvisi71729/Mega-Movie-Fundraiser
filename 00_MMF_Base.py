dba959d5833754aa76d37dd4ccff352ad654077d
Iszac Jarvis <jarvisi71729@masseyhigh.school.nz>
2021-02-05 12:09:03 +1300

    :smiling_imp:

---
+++ 00_MMF_Base.py
@@ -0,0 +1,30 @@
+#   import statements
+
+
+# functions go here
+
+
+# * * * * * Main routine * * * * *
+
+# Set up dictionaries / lists needed to hold data
+
+# Ask user if they have used the program before & show instructions if necessary
+
+# Loop to get ticket details
+
+    # Get name (can't be blank)
+
+    # Get age (between 12 and 130)
+
+    # Calculate ticket price
+
+    # Loop to ask for snacks
+
+    # Calculate snack price
+
+    # Ask for payment method (and apply surcharge if necessary)
+
+
+# Caculate Total sales and profit
+
+# Output data to text file
