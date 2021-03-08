945e8bf1fddef8af979a17a1b35fe115759af6d1
Iszac <58008716+jarvisi71729@users.noreply.github.com>
2021-03-09 09:55:22 +1300

    rhys is an arab

---
+++ 06_String_Checker.py
@@ -0,0 +1,24 @@
+
+
+# string checking functions, takes in
+# questions and list of valid responses
+def string_checker(question, to_check):
+
+    valid = False
+    while not valid:
+
+        response = input(question).lower()
+
+        if response in to_check:
+            return response
+
+        else:
+            for item in to_check:
+                # checks if response is the first letter of
+                # an item in the list
+                if response == item[0]:
+                    # note: returns the entire response
+                    # rather than just the first letter
+                    return item
+
+        print("Sorry that is not a valid response")
