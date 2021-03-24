d37e8fe5487f4e6dc9dd870e3cd28cf4d45d5f37
Iszac <58008716+jarvisi71729@users.noreply.github.com>
2021-03-24 14:39:39 +1300

    ez dubZZxz G

--- 08_snack_lists_expt_v2.py
+++ 08_snack_lists_expt_v2.py
@@ -1,6 +1,8 @@
+import pandas
+
 # Initialise snack lists...
 
-names = ['Rangi', 'Manaia', ' Talia', 'Arihi', 'Fetu']
+all_names = ['Rangi', 'Manaia', 'Talia', 'Arihi', 'Fetu']
 
 popcorn = []
 mms = []
@@ -10,7 +12,9 @@ orange_juice = []
 
 snack_lists = [popcorn, mms, pita_chips, water, orange_juice]
 
-snack_menu_dict = {
+# Data Frame Dictionary
+movie_data_dict = {
+    'Name': all_names,
     'Popcorn': popcorn,
     'Water': water,
     'Pita Chips': pita_chips,
@@ -43,12 +47,18 @@ for client_order in test_data:
         if len(item) > 0:
             to_find = item[1]
             amount = item[0]
-            add_list = snack_menu_dict[to_find]
+            add_list = movie_data_dict[to_find]
             add_list[-1] = amount
 
 print()
+print("Names:", all_names)
 print("Popcorn: ", snack_lists[0])
 print("M&Ms: ", snack_lists[1])
 print("Pita Chips: ", snack_lists[2])
 print("Water: ", snack_lists[3])
 print("Orange Juice: ", snack_lists[4])
+
+# print details...
+movie_frame = pandas.DataFrame(movie_data_dict)
+movie_frame = movie_frame.set_index('Name')
+print(movie_frame)
